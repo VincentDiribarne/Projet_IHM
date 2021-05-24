3c5634b17ba154ef9a71353260e86847f5cc7836
Vincent Diribarne <vdiribarnepro@gmail.com>
2021-05-24 18:22:52 +0200

    Ajout d'une classe util avec la méthode de déplacement, et Debut des controllers

--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -1,7 +1,8 @@
-package model;
+package controller;
 
-import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
@@ -9,13 +10,16 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import model.Util;
 
-public class Main extends Application {
+import java.io.IOException;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
+public class MenuController {
+
+    @FXML
+    private void Lancement() throws IOException {
         StackPane root = new StackPane();
-        root.getChildren().add(FXMLLoader.load(getClass().getResource("../view/Menu.fxml")));
+        root.getChildren().add(FXMLLoader.load(getClass().getResource("../view/game.fxml")));
         Rectangle rect = new Rectangle(800,500);
         rect.setArcHeight(10); //Permet de regler l'angle
         rect.setArcWidth(10);  //Permet de regler l'angle
@@ -23,19 +27,9 @@ public class Main extends Application {
         Scene scene = new Scene(root, 800, 500); //Mettre la même taille qu'en haut
         scene.setFill(Color.TRANSPARENT);
 
-        primaryStage.getIcons().add(new Image("./Ressources/Zombie.png"));
-        primaryStage.setTitle("Zombie Dice");
+        Stage primaryStage = new Stage();
         primaryStage.setScene(scene);
-        primaryStage.initStyle(StageStyle.TRANSPARENT);
-        primaryStage.setMinWidth(800);
-        primaryStage.setMinHeight(500);
-        primaryStage.setResizable(false);
         primaryStage.show();
-        Util.deplacementFenetre(root, primaryStage);
     }
 
-
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
