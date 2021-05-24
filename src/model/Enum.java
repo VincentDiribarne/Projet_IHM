9b7b6adb87fc3535e51ccffbe0e8fc41e36c112a
Vincent Diribarne <vdiribarnepro@gmail.com>
2021-05-24 19:58:47 +0200

    Ajout d'une enumération, fonction fermeture de la page, et ajout des joueurs

---
+++ src/model/Enum.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Enum {
+    public enum Difficulty {
+        easy,
+        medium,
+        hard;
+    }
+
+    public enum DiceFaces {
+        brain,
+        shotgun,
+        steps;
+    }
+}
